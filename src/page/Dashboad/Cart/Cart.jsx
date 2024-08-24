8817b9c011854242befd360cf8eebd8de0d5cee2
amithasanamithasan <amit35-1773@diu.edu.bd>
2024-08-24 22:04:49 +0600

    responsive desing table and image show the table

--- src/page/Dashboad/Cart/Cart.jsx
+++ src/page/Dashboad/Cart/Cart.jsx
@@ -33,7 +33,7 @@ const Cart = () => {
             <td>
               <div className="flex items-center gap-3">
                 <div className="avatar">
-                  <div className="mask mask-squircle h-12 w-12">
+                  <div className="mask mask-squircle h-20 w-20">
                     <img
                       src={item.image}
                       alt="Avatar Tailwind CSS Component" />
